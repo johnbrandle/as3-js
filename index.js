d4631dc02806637fcd585af4e1e567e954039419
John B <johnbrandle@gmail.com>
2023-03-14 14:14:34 -0500

    added compile

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 const as3_js = require('./_excluded/_generated/as3_js.node.1.js');
-const path = require('path');
-const fs = require('fs');
-
-as3_js.executeCommand('as3_js'); //compile the compiler using the compiler
